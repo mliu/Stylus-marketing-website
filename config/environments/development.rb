aea40166bb0e545d9729a8ac10e719ce73546166
mliu95 <michael.liu95@gmail.com>
2013-09-02 14:54:11 -0400

    font fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,10 @@ Penstore::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.paths << Rails.root.join("app", "assets", "fonts") 
+  config.assets.precompile += %w( .ttf )
 
   config.assets.enabled = true
   config.assets.compile= true
-  
+
 end
