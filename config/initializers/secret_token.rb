649be25946104d09ba5af2ac1798640938f03944
mliu95 <michael.liu95@gmail.com>
2013-09-02 12:38:30 -0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Penstore::Application.config.secret_key_base = '3c9fe3a62379211a3885d7648837a83ecf9ae1993ce4b0a261ea45138de216a96543849151df90f2a319acc012ce063acf5efe3d45539c0151320905c3cf25cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Penstore::Application.config.session_store :cookie_store, key: '_penstore_session'
